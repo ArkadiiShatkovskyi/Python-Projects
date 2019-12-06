1b86b194223f0f5fa1c61d67ed77a59310bda542
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2019-12-06 06:52:34 +0100

    PyQt test

---
+++ Python-Qt5-test/hello_world_pyqt5.py
@@ -0,0 +1,10 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+app = QApplication([])
+window = QWidget()
+layout = QVBoxLayout()
+layout.addWidget(QLabel('Test app'))
+layout.addWidget(QPushButton('Button1'))
+layout.addWidget(QPushButton('Button2'))
+window.setLayout(layout)
+window.show()
+app.exec_()
