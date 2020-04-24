b761c274f0ff97281413fc6a98dd95eb92aa4a37
Arkadii Shatkovskyi <arkadii.shatkovskyi@gmail.com>
2020-04-24 16:39:59 +0200

    Folders were renamed

--- Django/Django-Test/SiteTest/SiteTest/urls.py
+++ Django/Django-Test/SiteTest/SiteTest/urls.py
@@ -1,4 +1,4 @@
-"""BlogSite URL Configuration
+"""SiteTest URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
